a12f0eaa07fb617b30432ab546274bb444be35d4
Sannu Sharma <sannusharma02@gmail.com>
2025-03-05 11:43:06 +0530

    url

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server.');
+    const pathName = req.url;
+    console.log(pathName);
+    if(pathName === '/overview') {
+        res.end('This is the Overview');
+    } else if(pathName === '/product') {
+        res.end('This is the Overview');
+    } else {
+        res.writeHead(404,{
+            'Content-Type': 'text/html',
+            'my-owm-header':'hello world',
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 })
 
 server.listen(8000, '127.0.0.1',() => {
-    console.log('Listening on port 8080');
+    console.log('Listening on port 8000');
 });
