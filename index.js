cf4647db64345c9a12d2b92b8f535f23dddeb43c
Sannu Sharma <sannusharma02@gmail.com>
2025-03-05 10:28:44 +0530

    server

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
+const http = require('http');
 
-fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
-    if(err){
-        return console.log('Error!!');
-    }
-    fs.readFile('./txt/start.txt','utf-8',(err,data2)=>{
-        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',(err, data4)=>{
-                console.log(`Your file has been written`);
-            })
-        });
-    });
+const server = http.createServer((req, res) => {
+    console.log(req);
+    res.end('Hello from the server.');
+})
+
+server.listen(8000, '127.0.0.1',() => {
+    console.log('Listening on port 8080');
 });
-console.log(`Will read file!`);
