fca8ff2099f7de1364034a39e8561732c21ef22e
Sannu Sharma <sannusharma02@gmail.com>
2025-03-05 12:06:44 +0530

    Simple api

--- index.js
+++ index.js
@@ -2,22 +2,28 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const data =fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
+const dataObj = JSON.parse(data.toString());
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     console.log(pathName);
-    if(pathName === '/overview') {
+    if (pathName === '/overview') {
         res.end('This is the Overview');
-    } else if(pathName === '/product') {
+    } else if (pathName === '/product') {
         res.end('This is the Overview');
-    } else {
-        res.writeHead(404,{
+    } else if (pathName === '/api'){
+        res.writeHead(200,{'Content-Type': 'application/json'});
+        res.end(data)
+    } else{
+        res.writeHead(404, {
             'Content-Type': 'text/html',
-            'my-owm-header':'hello world',
+            'my-owm-header': 'hello world',
         });
         res.end('<h1>Page not found!</h1>');
     }
 })
 
-server.listen(8000, '127.0.0.1',() => {
+server.listen(8000, '127.0.0.1', () => {
     console.log('Listening on port 8000');
 });
