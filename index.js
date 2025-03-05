71d985b1edf34c65372bb4e21b8c4cdc6f8ae661
Sannu Sharma <sannusharma02@gmail.com>
2025-03-05 10:10:48 +0530

    async

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Happy developing ✨')
+const fs = require('fs');
+
+fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+    fs.readFile('./txt/start.txt','utf-8',(err,data2)=>{
+        fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',(err, data4)=>{
+                console.log(`Your file has been written`);
+            })
+        });
+    });
+});
+console.log(`Will read file!`);
